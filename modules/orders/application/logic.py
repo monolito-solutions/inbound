03e2d720c42e48afc33fb1a2fee41fdfdb249e97
ElReyZero <juan.romero1201@gmail.com>
2023-03-12 15:57:02 +0000

    Fix: Add auto uuid

--- modules/orders/application/logic.py
+++ modules/orders/application/logic.py
@@ -9,15 +9,35 @@ from config.db import get_db
 import utils
 import json
 import traceback
-
-def create_order(order:dict):
+import uuid
+def create_order(order):
+    order.order_id = str(uuid.uuid4())
     db = get_db()
     order = detect_order_version(order)
     try:
         repository = OrdersRepositorySQLAlchemy(db)
         repository.create(order)
     except IntegrityError:
-        raise BaseAPIException(f"Error creating order, primary key integrity violated (Duplicate ID)", 400)
+        dispatcher = Dispatcher()
+        event_payload = OrderCreatedPayload(
+            order_id = str(order.order_id),
+            customer_id = str(order.customer_id),
+            order_date = str(order.order_date),
+            order_status = "Failed To Create: Duplicate order entry",
+            order_items = json.dumps(order.order_items) if type(order.order_items != str) else order.order_items,
+            order_total = float(order.order_total),
+            order_version = int(order.order_version)
+        )
+
+        event = EventOrderCreated(
+            time = utils.time_millis(),
+            ingestion = utils.time_millis(),
+            datacontenttype = OrderCreatedPayload.__name__,
+            data_payload = event_payload,
+            type = ""
+        )
+        dispatcher.publish_message(event, "order-events")
+        return
     except Exception as e:
         raise BaseAPIException(f"Error creating order: {e}", 500)
     finally:
